3382102dbd39d95cb43271d2d263153c24590f32
Eva <17610603221@163.com>
2022-08-26 19:20:39 +0800

    add ExtraStorage

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -16,12 +16,14 @@ contract StorageFactory {
     {
         // Address
         // ABI - Application Binary Interface
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
-        simpleStorage.store(_simpleStorageNumber);
+        // SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        // simpleStorage.store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
-        return simpleStorage.retrieve();
+        // SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        // return simpleStorage.retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
