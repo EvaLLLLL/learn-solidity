b75eeb01fed32ec4ff4e0e154e342ad7d0c90786
Eva <17610603221@163.com>
2022-08-28 20:02:48 +0800

    add PriceConverter

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,56 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
-
-  function description() external view returns (string memory);
-
-  function version() external view returns (uint256);
-
-  function getRoundData(uint80 _roundId)
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-
-  function latestRoundData()
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-}
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 50;
+    uint256 public minimumUsd = 50 * (10**18);
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFounded;
 
     function fund() public payable {
-        require(msg.value >= minimumUsd, "Didn't send enough!");
-    }
-
-    function getPrice() public {
-        // ABI, 
-        // Address 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        return priceFeed.version();
-    }
-
-    function getConversionRate() public {
-        
+        require(
+            msg.value.getConversionRate() >= minimumUsd,
+            "Didn't send enough!"
+        ); // 18 decimals
+        funders.push(msg.sender);
+        addressToAmountFounded[msg.sender] = msg.value;
     }
 
     // function withdraw() {}
